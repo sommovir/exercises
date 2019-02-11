5bd8983ef67e874f5b8c437931e8e62128c38048
Luca <Luca@150.146.65.137>
2019-02-11 17:50:37 +0100

    init test

---
+++ src/esercizi/blocco1/easy/Easy1.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package esercizi.blocco1.easy;
+
+/**
+ *
+ * @author Luca Coraci <luca.coraci@istc.cnr.it> ISTC-CNR
+ */
+public class Easy1 {
+    
+    /**
+     * return the sum of a and b
+     * @param a
+     * @param b
+     * @return 
+     */
+    public int somma(int a, int b){
+        return -1;
+    }
+    
+    /**
+     * returns how many text block are separated by <separator> 
+     * Example 1: "ciao,a,tutti" with separator "," contains 3 blocks
+     * Example 2: "mario/gino and separator "/" contains 2 blocks
+     * Example 3: "alfonso-" with separator  "-" contains 1 block.
+     * Example 4: "mario,,,gino,pina,," and "," contains 5 blocks. (IMPORTANT: final empty blocks doesnt counts)
+     * Example 4B: "mario,gino,,,,,,,,,,,,,,,,,," and "," contains 2 blocks
+     * Example 4C: "mario,,gino,,,,,,,,,,,,," and "," contains 3 blocks. 
+     * Example 4D: ",,,,,,,,,," and "," contains 0 blocks because all blocks are empty
+     * Example 5: "mario,gino,strada" and "-" separator contains 1 block even if "-" separator is never encountered. But it counts as a unique block. 
+     * Note: empty string will result always as a one block. But if the input is nulle, blocks are zero.  
+     * 
+     * IMPORTANT !!!!!!!!!!!! DO NOT USE "split" method of String class. 
+     * 
+     * @param text
+     * @param separator
+     * @return 
+     */
+    public int countBlock(String text, String separator){
+
+        return -1;
+    }
+    
+    public static void main(String[] args) {
+        System.out.println("test");
+        String t = "mario,gino,,,,,,,,,,,,,";
+        String[] split = t.split(",");
+        for (String string : split) {
+            System.out.println("BLOCCO: "+string);
+        }
+        System.out.println("blocchi = "+split.length);
+        
+    }
+    
+}
---
+++ test/Easy1Test.java
@@ -0,0 +1,255 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import esercizi.blocco1.easy.Easy1;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.FixMethodOrder;
+import org.junit.runners.MethodSorters;
+
+/**
+ *
+ * @author Luca Coraci <luca.coraci@istc.cnr.it> ISTC-CNR
+ */
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class Easy1Test {
+
+    private static int totalTest = 0;
+    private static int successTest = 0;
+    private static int failedTest = 0;
+
+    public Easy1Test() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+        System.out.println("------------------------------------------------");
+        System.out.println("Total test: "+totalTest);
+        System.out.println("SUCCESS: "+successTest+"/"+totalTest);
+        System.out.println("FAILED: "+failedTest);
+    }
+
+    @Before
+    public void setUp() {
+        totalTest++;
+    }
+
+    @After
+    public void tearDown() {
+
+    }
+
+    @Test
+    public void testSomma() {
+        System.out.print("[testing somma][1]..");
+        int a = 3;
+        int b = 5;
+        Easy1 instance = new Easy1();
+        int result = instance.somma(a, b);
+        int expectedResult = 8;
+        try {
+            assertEquals("[testing somma][ERRORE] 3 + 5 != " + result, expectedResult, result);
+        } catch (AssertionError e) {
+            System.out.println("\t\t\t[FAIL]");
+            failedTest++;
+            throw e;
+        }
+        System.out.println("\t\t\t[OK]");
+        successTest++;
+
+    }
+
+    @Test
+    public void testSomma2() {
+        System.out.print("[testing somma][2]..");
+        int a = -3;
+        int b = 5;
+        Easy1 instance = new Easy1();
+        int result = instance.somma(a, b);
+        int expectedResult = 2;
+        try {
+            assertEquals("[testing somma2][ERRORE] -3 + 5 != " + result, expectedResult, result);
+        } catch (AssertionError e) {
+            System.out.println("\t\t\t[FAIL]");
+            failedTest++;
+            throw e;
+        }
+        System.out.println("\t\t\t[OK]");
+        successTest++;
+        //fail("[testing somma][ERRORE] 3 + 5 != "+result);
+
+    }
+    
+    
+    @Test
+    public void testCountBlock1() {
+        System.out.print("[testing countblock][1]..");
+        Easy1 instance = new Easy1();
+        String text = "ciao,belli";
+        String separator =",";
+        int result = instance.countBlock(text,separator);
+        int expectedResult = 2;
+        try {
+            assertEquals("[testing countblock][ERRORE] examining string= "+text+" and separator = '"+separator+"'", expectedResult, result);
+        } catch (AssertionError e) {
+            System.out.println("\t\t[FAIL]");
+            failedTest++;
+            throw e;
+        }
+        System.out.println("\t[OK]");
+        successTest++;
+
+    }
+    
+    @Test
+    public void testCountBlock2() {
+        System.out.print("[testing countblock][2]..");
+        Easy1 instance = new Easy1();
+        String text = "paolo-mario-pedro-alifrizzi";
+        String separator ="-";
+        int result = instance.countBlock(text,separator);
+        int expectedResult = 4;
+        try {
+            assertEquals("[testing countblock][ERRORE] examining string= "+text+" and separator = '"+separator+"'", expectedResult, result);
+        } catch (AssertionError e) {
+            System.out.println("\t\t[FAIL]");
+            failedTest++;
+            throw e;
+        }
+        System.out.println("\t[OK]");
+        successTest++;
+
+    }
+    
+    @Test
+    public void testCountBlock3() {
+        System.out.print("[testing countblock][3]..");
+        Easy1 instance = new Easy1();
+        String text = "paolo-mario-pedro-alifrizzi";
+        String separator =",";
+        int result = instance.countBlock(text,separator);
+        int expectedResult = 1;
+        try {
+            assertEquals("[testing countblock][ERRORE] examining string= "+text+" and separator = '"+separator+"'", expectedResult, result);
+        } catch (AssertionError e) {
+            System.out.println("\t\t[FAIL]");
+            failedTest++;
+            throw e;
+        }
+        System.out.println("\t[OK]");
+        successTest++;
+
+    }
+    
+    @Test
+    public void testCountBlock4() {
+        System.out.print("[testing countblock][4]..");
+        Easy1 instance = new Easy1();
+        String text = "";
+        String separator =",";
+        int result = instance.countBlock(text,separator);
+        int expectedResult = 1;
+        try {
+            assertEquals("[testing countblock][ERRORE] examining string= "+text+" and separator = '"+separator+"'", expectedResult, result);
+        } catch (AssertionError e) {
+            System.out.println("\t\t[FAIL]");
+            failedTest++;
+            throw e;
+        }
+        System.out.println("\t[OK]");
+        successTest++;
+
+    }
+    
+    @Test
+    public void testCountBlock5() {
+        System.out.print("[testing countblock][5]..");
+        Easy1 instance = new Easy1();
+        String text = null;
+        String separator =",";
+        int result = instance.countBlock(text,separator);
+        int expectedResult = 0;
+        try {
+            assertEquals("[testing countblock][ERRORE] examining string= "+text+" and separator = '"+separator+"'", expectedResult, result);
+        } catch (AssertionError e) {
+            System.out.println("\t\t[FAIL]");
+            failedTest++;
+            throw e;
+        }
+        System.out.println("\t[OK]");
+        successTest++;
+
+    }
+    
+    @Test
+    public void testCountBlock6() {
+        System.out.print("[testing countblock][6]..");
+        Easy1 instance = new Easy1();
+        String text = "ciao,bella-come stai, tutt'ok ?,,sisi,";
+        String separator =",";
+        int result = instance.countBlock(text,separator);
+        int expectedResult = 5;
+        try {
+            assertEquals("[testing countblock][ERRORE] examining string= "+text+" and separator = '"+separator+"'", expectedResult, result);
+        } catch (AssertionError e) {
+            System.out.println("\t\t[FAIL]");
+            failedTest++;
+            throw e;
+        }
+        System.out.println("\t[OK]");
+        successTest++;
+
+    }
+    
+    @Test
+    public void testCountBlock7() {
+        System.out.print("[testing countblock][7]..");
+        Easy1 instance = new Easy1();
+        String text = "uno,,,,due,,tre,,,";
+        String separator =",";
+        int result = instance.countBlock(text,separator);
+        int expectedResult = 7;
+        try {
+            assertEquals("[testing countblock][ERRORE] examining string= "+text+" and separator = '"+separator+"'", expectedResult, result);
+        } catch (AssertionError e) {
+            System.out.println("\t\t[FAIL]");
+            failedTest++;
+            throw e;
+        }
+        System.out.println("\t[OK]");
+        successTest++;
+
+    }
+    
+    @Test
+    public void testCountBlock8() {
+        System.out.print("[testing countblock][7]..");
+        Easy1 instance = new Easy1();
+        String text = ",,,,,,,,,,,";
+        String separator =",";
+        int result = instance.countBlock(text,separator);
+        int expectedResult = 0;
+        try {
+            assertEquals("[testing countblock][ERRORE] examining string= "+text+" and separator = '"+separator+"'", expectedResult, result);
+        } catch (AssertionError e) {
+            System.out.println("\t\t[FAIL]");
+            failedTest++;
+            throw e;
+        }
+        System.out.println("\t[OK]");
+        successTest++;
+
+    }
+
+}
