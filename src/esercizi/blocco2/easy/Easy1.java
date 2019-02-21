9484935bcfadb006474adc77b41b61ad0da230e0
Luca <Luca@150.146.65.137>
2019-02-21 16:55:21 +0100

    blocco 2 easy 1

---
+++ src/esercizi/blocco2/easy/Easy1.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package esercizi.blocco2.easy;
+
+/**
+ *
+ * @author Luca Coraci <luca.coraci@istc.cnr.it> ISTC-CNR
+ */
+public class Easy1 {
+
+    /**
+     * Restituire la media dei n numeri dati in input.
+     *
+     * @param n
+     * @return
+     */
+    public double media(int... n) {
+        return -1d;
+    }
+
+    /**
+     * ritorna il numero di zeri contenuti nell'array in input
+     *
+     * @param array
+     * @return
+     */
+    public int countZero(int[] array) {
+        return -1;
+    }
+
+    /**
+     * dati due array di interi in input di uguale dimensione (viceversa ritorna
+     * un eccezione) ritorna un array che è la somma dei precedenti (c[i] = a[i]+b[i] )
+     *
+     * @param a
+     * @param b
+     * @return
+     */
+    public int[] sumArrays(int[] a, int[] b) throws Exception{
+        return null;
+    }
+
+    /**
+     * Data una stringa contenente i dati di una persona nel formato
+     * NOME:COGNOME:AGE, il metodo parsa la stringa e ritorna l'oggetto Persona
+     * corrispondente.
+     *
+     * @param string_person
+     * @return
+     */
+    public Persona generatePersonaFromString(String string_person) {
+        return null;
+    }
+
+}
---
+++ src/esercizi/blocco2/easy/Persona.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package esercizi.blocco2.easy;
+
+/**
+ * NON MODIFICARE
+ * @author Luca Coraci <luca.coraci@istc.cnr.it> ISTC-CNR
+ */
+public class Persona {
+    
+    private String nome;
+    private String cognome;
+    private int age;
+
+    public Persona() {
+    }
+
+    public Persona(String nome, String cognome, int age) {
+        this.nome = nome;
+        this.cognome = cognome;
+        this.age = age;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCognome() {
+        return cognome;
+    }
+
+    public void setCognome(String cognome) {
+        this.cognome = cognome;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(obj == null){
+            return false;
+        }
+        if(obj instanceof Persona){
+            Persona p = (Persona)obj;
+            if(this.nome.equals(p.nome) && this.cognome.equals(p.cognome) && this.age == p.age){
+                return true;
+            }else{
+                return false;
+            }
+        }else{
+            return false;
+        }
+    }
+    
+    
+    
+    
+    
+}
