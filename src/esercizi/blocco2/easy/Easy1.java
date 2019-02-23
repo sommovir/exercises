1dc2b6e51419aafa39a22460a62e540abcd27b93
User <User@User-PC>
2019-02-23 12:07:02 +0100

    solution easy1 luca

--- src/esercizi/blocco2/easy/Easy1.java
+++ src/esercizi/blocco2/easy/Easy1.java
@@ -18,7 +18,14 @@ public class Easy1 {
      * @return
      */
     public double media(int... n) {
-        return -1d;
+        if(n == null || n.length == 0){
+            return 0;
+        }
+        int sum = 0;
+        for (int i : n) {
+            sum+=i;
+        }
+        return sum/n.length;
     }
 
     /**
@@ -28,7 +35,11 @@ public class Easy1 {
      * @return
      */
     public int countZero(int[] array) {
-        return -1;
+        int zeros = 0;
+        for (int i : array) {
+            if (i == 0) zeros++;
+        }
+        return zeros;
     }
 
     /**
@@ -40,7 +51,15 @@ public class Easy1 {
      * @return
      */
     public int[] sumArrays(int[] a, int[] b) throws Exception{
-        return null;
+        if(a.length != b.length){
+            throw new Exception();
+        }
+        int [] c = new int[a.length];
+        for (int i = 0; i < a.length; i++) {
+            
+            c[i] = a[i] + b[i];
+        }
+        return c;
     }
 
     /**
