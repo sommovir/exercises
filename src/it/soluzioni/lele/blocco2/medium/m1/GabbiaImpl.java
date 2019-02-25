3efd83d83faf31efe84cd0c9e4175a3ac67d5a59
User <User@User-PC>
2019-02-25 02:51:13 +0100

    fix GabbiaImpl javadoc

--- src/it/soluzioni/lele/blocco2/medium/m1/GabbiaImpl.java
+++ src/it/soluzioni/lele/blocco2/medium/m1/GabbiaImpl.java
@@ -19,32 +19,37 @@ import java.util.List;
 @Solution
 public class GabbiaImpl implements Gabbia{
     
-    @Override
+    /**
+     * svuota complemente la gabbia riportandola allo stato iniziale. 
+     */
     public void clear() {
+        throw new UnsupportedOperationException();
     }
     
     /**
      * aggiunge un cane alla gabbia
      * La gabbia però deve essere fatta in modo che non ci possano essere più cani di un solo padrone. 
+     * Nel caso si tenta di inserire un cane con un padrone già esistente, la funzione ignora semplicemente e non lancia eccezioni
+     * Nel caso invece si tenta di inserire un oggetto vuoto, il metodo lancerà una NullPointerException contenente il testuale messaggio: 'Input Nullo'
      * @param cane 
      */
-    public void addDog(Dog cane) throws NullPointerException{
-        
+    public void addDog(Dog cane) throws NullPointerException {
+        throw new UnsupportedOperationException();
     }
     
     /**
      * aggiunge una pecora alla gabbia
+     * il metodo ignora input nulli o oggetti Sheep con il colore non settato o stringa vuota. 
      * @param pecora 
      */
-    public void addPecora(Sheep pecora){
-        
+    public void addPecora(Sheep pecora) {
+        throw new UnsupportedOperationException();
     }
-    
     /**
      * restituisce tutti gli animali contenuti nella gabbia
      * @return 
      */
-    public List<Animal> getAnimaliInGabbia(){
+    public List<Animal> getAnimaliInGabbia() {
         throw new UnsupportedOperationException();
     }
     
@@ -52,24 +57,23 @@ public class GabbiaImpl implements Gabbia{
      * restituisce il totale dei cani contenuti nella gabbia
      * @return 
      */
-    public int countDogs(){
-        
+    public int countDogs() {
         throw new UnsupportedOperationException();
     }
     
     /**
      * rimuove dalla gabbia tutti i cani
      */
-    public void removeAllDog(){
+    public void removeAllDog() {
         throw new UnsupportedOperationException();
     }
     
     /**
-     * ritorna la pecora con il colore desiderato passato in input, null se la pecora non è trovata o se l'input è nullo o stringa vuota. 
+     * ritorna le pecore con il colore desiderato passato in input, Se l'input è una stringa vuota o null o non trova nulla ritorna una lista vuota.  
      * @param color
      * @return 
      */
-    public List<Sheep> findSheepsByColor(String color){
+    public List<Sheep> findSheepsByColor(String color) {
         throw new UnsupportedOperationException();
     }
 
